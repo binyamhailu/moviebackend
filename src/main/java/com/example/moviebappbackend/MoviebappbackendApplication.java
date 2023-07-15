58b2a1e85f48da2da6df31ed6d3172d2f63742d5
Binyam.Hailu <binyam.hailu@partner.safaricom.et>
2023-07-15 18:27:13 +0300

    initial boilerplate

---
+++ src/main/java/com/example/moviebappbackend/MoviebappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviebappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviebappbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviebappbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moviebappbackend/MoviebappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviebappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviebappbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
