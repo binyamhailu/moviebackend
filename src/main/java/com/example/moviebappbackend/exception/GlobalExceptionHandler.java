e5cfac0817e4871d10206f29a8e9ff4d5c4521d2
mengestu <binyam.hailu@partner.safaricom.et>
2023-07-16 14:53:02 +0300

    Added Exception handler for field validation

--- src/main/java/com/example/moviebappbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/moviebappbackend/exception/GlobalExceptionHandler.java
@@ -2,24 +2,15 @@ package com.example.moviebappbackend.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.postgresql.util.PSQLException;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
-import jakarta.validation.Path;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 @RestControllerAdvice
